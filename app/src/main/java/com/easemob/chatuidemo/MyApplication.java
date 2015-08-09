3ff11d0341b971d58eb70c1cb46e38d6509d32b8
Jekton <ljtong64@outlook.com>
2015-08-09 17:22:02 +0800

    Fix bug that the UniversalImageLoader didn't init

--- app/src/main/java/com/easemob/chatuidemo/MyApplication.java
+++ app/src/main/java/com/easemob/chatuidemo/MyApplication.java
@@ -13,8 +13,6 @@
  */
 package com.easemob.chatuidemo;
 
-import java.util.Map;
-
 import android.app.Application;
 import android.content.Context;
 
@@ -23,6 +21,12 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.easemob.EMCallBack;
 import com.easemob.chatuidemo.domain.User;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+import java.util.Map;
 
 public class MyApplication extends Application {
 
@@ -30,57 +34,59 @@ public class MyApplication extends Application {
 	private static MyApplication instance;
 	// login user name
 	public final String PREF_USERNAME = "username";
-	
+
 	/**
 	 * 当前用户nickname,为了苹果推送不是userid而是昵称
 	 */
 	public static String currentUserNick = "";
 	public static DemoHXSDKHelper hxSDKHelper = new DemoHXSDKHelper();
-	
 
-    private static RequestQueue requestQueue;
+
+	private static RequestQueue requestQueue;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-        applicationContext = this;
-        instance = this;
-        
-        requestQueue = Volley.newRequestQueue(this);
-        requestQueue.start();
+		applicationContext = this;
+		instance = this;
 
-        /**
-         * this function will initialize the HuanXin SDK
-         * 
-         * @return boolean true if caller can continue to call HuanXin related APIs after calling onInit, otherwise false.
-         * 
-         * 环信初始化SDK帮助函数
-         * 返回true如果正确初始化，否则false，如果返回为false，请在后续的调用中不要调用任何和环信相关的代码
-         * 
-         * for example:
-         * 例子：
-         * 
-         * public class DemoHXSDKHelper extends HXSDKHelper
-         * 
-         * HXHelper = new DemoHXSDKHelper();
-         * if(HXHelper.onInit(context)){
-         *     // do HuanXin related work
-         * }
-         */
-        hxSDKHelper.onInit(applicationContext);
+		requestQueue = Volley.newRequestQueue(this);
+		requestQueue.start();
+
+		/**
+		 * this function will initialize the HuanXin SDK
+		 *
+		 * @return boolean true if caller can continue to call HuanXin related APIs after calling onInit, otherwise false.
+		 *
+		 * 环信初始化SDK帮助函数
+		 * 返回true如果正确初始化，否则false，如果返回为false，请在后续的调用中不要调用任何和环信相关的代码
+		 *
+		 * for example:
+		 * 例子：
+		 *
+		 * public class DemoHXSDKHelper extends HXSDKHelper
+		 *
+		 * HXHelper = new DemoHXSDKHelper();
+		 * if(HXHelper.onInit(context)){
+		 *     // do HuanXin related work
+		 * }
+		 */
+		hxSDKHelper.onInit(applicationContext);
+
+		initImageLoader(applicationContext);
 	}
 
 	public static MyApplication getInstance() {
 		return instance;
 	}
- 
+
 	/**
 	 * 获取内存中好友user list
 	 *
 	 * @return
 	 */
 	public Map<String, User> getContactList() {
-	    return hxSDKHelper.getContactList();
+		return hxSDKHelper.getContactList();
 	}
 
 	/**
@@ -89,7 +95,7 @@ public class MyApplication extends Application {
 	 * @param contactList
 	 */
 	public void setContactList(Map<String, User> contactList) {
-	    hxSDKHelper.setContactList(contactList);
+		hxSDKHelper.setContactList(contactList);
 	}
 
 	/**
@@ -98,7 +104,7 @@ public class MyApplication extends Application {
 	 * @return
 	 */
 	public String getUserName() {
-	    return hxSDKHelper.getHXId();
+		return hxSDKHelper.getHXId();
 	}
 
 	/**
@@ -113,10 +119,10 @@ public class MyApplication extends Application {
 	/**
 	 * 设置用户名
 	 *
-	 * @param user
+	 * @param username
 	 */
 	public void setUserName(String username) {
-	    hxSDKHelper.setHXId(username);
+		hxSDKHelper.setHXId(username);
 	}
 
 	/**
@@ -126,7 +132,7 @@ public class MyApplication extends Application {
 	 * @param pwd
 	 */
 	public void setPassword(String pwd) {
-	    hxSDKHelper.setPassword(pwd);
+		hxSDKHelper.setPassword(pwd);
 	}
 
 	/**
@@ -134,22 +140,40 @@ public class MyApplication extends Application {
 	 */
 	public void logout(final EMCallBack emCallBack) {
 		// 先调用sdk logout，在清理app中自己的数据
-	    hxSDKHelper.logout(emCallBack);
+		hxSDKHelper.logout(emCallBack);
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	// *****************************************************
 	// Jekton
 	// *****************************************************
-	 public static <T> void addToRequestQueue(Request<T> request) {
-	        requestQueue.add(request);
-	    }
+	public static <T> void addToRequestQueue(Request<T> request) {
+		requestQueue.add(request);
+	}
 
-	    public static void cancelRequest(Object tag) {
-	        requestQueue.cancelAll(tag);
-	    }
+	public static void cancelRequest(Object tag) {
+		requestQueue.cancelAll(tag);
+	}
+
+
+	private static void initImageLoader(Context context) {
+		// This configuration tuning is custom. You can tune every option, you may tune some of them,
+		// or you can create default configuration by
+		//  ImageLoaderConfiguration.createDefault(this);
+		// method.
+		ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(context);
+		config.threadPriority(Thread.NORM_PRIORITY - 2);
+		config.denyCacheImageMultipleSizesInMemory();
+		config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
+		config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
+		config.tasksProcessingOrder(QueueProcessingType.LIFO);
+		config.writeDebugLogs(); // Remove for release app
+
+		// Initialize ImageLoader with configuration.
+		ImageLoader.getInstance().init(config.build());
+	}
 }
