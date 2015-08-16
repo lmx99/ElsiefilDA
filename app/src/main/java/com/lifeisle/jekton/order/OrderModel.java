a7925e9db426b33ab521aeb5a6707c85081dcbe5
Jekton <ljtong64@outlook.com>
2015-08-16 10:28:53 +0800

    Set decoded format for the scanner

--- app/src/main/java/com/lifeisle/jekton/order/OrderModel.java
+++ app/src/main/java/com/lifeisle/jekton/order/OrderModel.java
@@ -270,7 +270,7 @@ public class OrderModel {
 
     /**
      *
-     * @param orderCode EAN-13 orderCode
+     * @param orderCode code 128 format orderCode
      */
     public void addOrder(String orderCode) {
         if (jcat_id < 0) {
